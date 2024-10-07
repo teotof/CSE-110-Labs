b3258915791660d0b867055216d60c9ca0853313
teotof <hadjitot@tcd.ie>
2024-10-07 14:12:35 -0700

    Part 4.1 Done

---
+++ CSE 110 - LAB 2/my-app/src/App.tsx
@@ -0,0 +1,34 @@
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+import { dummyNotesList } from "./constants"; // Import the dummyNotesList from the appropriate module
+import './App.css';
+
+function App() {
+  return (
+    <div className='app-container'>
+      <form className="note-form">
+        <div><input placeholder="Note Title"></input></div>
+
+        <div><textarea></textarea></div>
+
+        <div><button type="submit">Create Note</button></div>
+      </form>
+
+      <div className="notes-grid">
+        {dummyNotesList.map((note) => (
+          <div
+            key={note.id}
+            className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2> {note.title} </h2>
+            <p> {note.content} </p>
+            <p> {note.label} </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
